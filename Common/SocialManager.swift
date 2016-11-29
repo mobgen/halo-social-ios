296d55055119f5408375e981f10a839025656e3e
Borja Santos-Diez <borja.santos@mobgen.com>
2016-11-29 11:53:20 +0100

    Added some more structure to the Framework

--- Common/SocialManager.swift
+++ Common/SocialManager.swift
@@ -9,11 +9,6 @@
 import Foundation
 import Halo
 
-@objc
-public enum SocialNetwork: Int {
-    case halo, facebook, google
-}
-
 @objc(HaloSocialManager)
 public class SocialManager: NSObject, HaloManager {
     
@@ -22,11 +17,11 @@ public class SocialManager: NSObject, HaloManager {
         
     }
     
-    public func login(network: SocialNetwork, authProfile: AuthProfile?, completionHandler handler: (SocialProfile?, NSError?) -> Void) -> Void {
+    public func register(authProfile: AuthProfile, userProfile: UserProfile, completionHandler handler: (User?, NSError?) -> Void) -> Void {
         
     }
     
-    public func register(authProfile: AuthProfile, userProfile: UserProfile, completionHandler handler: (SocialProfile?, NSError?) -> Void) -> Void {
+    public func loginWithHalo(username: String, password: String) {
         
     }
     
---
+++ Google/SocialManager+Google.swift
@@ -0,0 +1,17 @@
+//
+//  SocialManager+Google.swift
+//  HaloSocial
+//
+//  Created by Borja Santos-Díez on 29/11/16.
+//  Copyright © 2016 Mobgen Technology. All rights reserved.
+//
+
+import Foundation
+
+extension SocialManager {
+    
+    public func loginWithGoogle() {
+        
+    }
+    
+}
