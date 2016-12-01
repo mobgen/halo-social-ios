2a95f7b788f07c700c1367e132b353f8f7bac2c2
Miguel López <miguel.lopez@mobgen.com>
2016-12-01 10:39:17 +0100

    Reorganize code of FacebookSocialAddon

--- Facebook/FacebookSocialAddon.swift
+++ Facebook/FacebookSocialAddon.swift
@@ -14,58 +14,9 @@ import FacebookLogin
 @objc(HaloFacebookSocialAddon)
 class FacebookSocialAddon : NSObject, Halo.DeeplinkingAddon, SocialProvider {
     
-    var addonName: String = "FacebookSocialAddon"
-    
-    func setup(haloCore core: CoreManager, completionHandler handler: ((Addon, Bool) -> Void)?) {
-        
-    }
-    
-    func startup(haloCore core: CoreManager, completionHandler handler: ((Addon, Bool) -> Void)?) {
-        
-    }
-    
-    func didRegisterAddon(haloCore core: CoreManager) {
-    }
-    
-    func willRegisterAddon(haloCore core: CoreManager) {
-        
-    }
-    
-    func application(_ app: UIApplication, open url: URL, options: [UIApplicationOpenURLOptionsKey : Any]) -> Bool {
-        return false
-    }
-    
-    func application(_ application: UIApplication, open url: URL, sourceApplication: String?, annotation: Any) -> Bool {
-        return false
-    }
-    
-    func login(viewController: UIViewController? = nil, completionHandler handler: @escaping (User?, NSError?) -> Void) {
-        SocialManager.loginWithFacebook(viewController: viewController,
-                                        completionHandler: handler)
-    }
-    
-    private func userParser(_ data: Any?) -> User? {
-        if let dict = data as? [String: Any] {
-            return User.fromDictionary(dict: dict)
-        }
-        return nil
-    }
-    
-    public func authenticate(authProfile: AuthProfile, completionHandler handler: (User?, NSError?) -> Void) {
-        let request = Halo.Request<User>(router: Router.loginUser(authProfile.toDictionary()))
-        try! request.responseParser(parser: userParser).responseObject(completionHandler: handler)
-    }
-}
-
-extension SocialManager {
-    
-    static let facebookProvider: FacebookSocialProvider = {
-        return FacebookSocialProvider()
-    }()
-    
-    enum SocialManagerError {
-        case FacebookError
-        case FacebookCancelled
+    enum FacebookSocialAddonError {
+        case Error
+        case Cancelled
         
         static var errorDomain: String {
             return "SocialManagerError"
@@ -90,6 +41,47 @@ extension SocialManager {
         }
     }
     
+    var addonName: String = "FacebookSocialAddon"
+    
+    // MARK : Addon methods.
+    
+    func setup(haloCore core: CoreManager, completionHandler handler: ((Addon, Bool) -> Void)?) { }
+    
+    func startup(haloCore core: CoreManager, completionHandler handler: ((Addon, Bool) -> Void)?) { }
+    
+    func willRegisterAddon(haloCore core: CoreManager) { }
+    
+    func didRegisterAddon(haloCore core: CoreManager) { }
+    
+    // MARK : DeeplinkingAddon methods.
+    
+    func application(_ app: UIApplication, open url: URL, options: [UIApplicationOpenURLOptionsKey : Any]) -> Bool {
+        return false
+    }
+    
+    func application(_ application: UIApplication, open url: URL, sourceApplication: String?, annotation: Any) -> Bool {
+        return false
+    }    
+    
+    private func userParser(_ data: Any?) -> User? {
+        if let dict = data as? [String: Any] {
+            return User.fromDictionary(dict: dict)
+        }
+        return nil
+    }
+    
+    public func authenticate(authProfile: AuthProfile, completionHandler handler: (User?, NSError?) -> Void) {
+        let request = Halo.Request<User>(router: Router.loginUser(authProfile.toDictionary()))
+        try! request.responseParser(parser: userParser).responseObject(completionHandler: handler)
+    }
+}
+
+extension SocialManager {
+    
+    private var facebookSocialAddon: FacebookSocialAddon? {
+        return Manager.core.addons.filter { $0 is FacebookSocialAddon }.first as? FacebookSocialAddon
+    }
+    
     func loginWithFacebook(viewController: UIViewController? = nil, completionHandler handler: @escaping (User?, NSError?) -> Void) {
         
         // 1. Start login with facebook
@@ -101,15 +93,15 @@ extension SocialManager {
             case .failed(let error):
                 // 2.a. Show error.
                 print(error)
-                handler(nil, NSError(domain: SocialManagerError.errorDomain,
-                                     code: SocialManagerError.FacebookError.errorCode,
+                handler(nil, NSError(domain: FacebookSocialAddonError.errorDomain,
+                                     code: FacebookSocialAddonError.Error.errorCode,
                                      userInfo: [NSLocalizedDescriptionKey: error.localizedDescription]))
             case .cancelled:
                 // 2.b. Show error.
                 print("User cancelled login")
-                handler(nil, NSError(domain: SocialManagerError.errorDomain,
-                                     code: SocialManagerError.FacebookCancelled.errorCode,
-                                     userInfo: SocialManagerError.FacebookCancelled.errorUserInfo))
+                handler(nil, NSError(domain: FacebookSocialAddonError.errorDomain,
+                                     code: FacebookSocialAddonError.Cancelled.errorCode,
+                                     userInfo: FacebookSocialAddonError.Cancelled.errorUserInfo))
             case .success(let grantedPermissions, let declinedPermissions, let accessToken):
                 // 2.c. Ask for rest of data.
                 print("Logged in!")
