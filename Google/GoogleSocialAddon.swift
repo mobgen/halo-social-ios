c3f43def784a83b10a44350e9ad939f7cac7a392
Borja Santos-Diez <borja.santos@mobgen.com>
2016-12-02 12:46:32 +0100

    Complete the authentication in the GoogleSocialAddon

--- Google/GoogleSocialAddon.swift
+++ Google/GoogleSocialAddon.swift
@@ -16,6 +16,7 @@ import Firebase
 public class GoogleSocialAddon: NSObject, DeeplinkingAddon, SocialProvider, GIDSignInDelegate {
     
     public var addonName: String = "GoogleSocialAddon"
+    var completionHandler: (User?, NSError?) -> Void = { _, _ in }
     
     public func setup(haloCore core: CoreManager, completionHandler handler: ((Addon, Bool) -> Void)?) {
         handler?(self, true)
@@ -59,36 +60,49 @@ public class GoogleSocialAddon: NSObject, DeeplinkingAddon, SocialProvider, GIDS
     
     public func sign(_ signIn: GIDSignIn!, didSignInFor user: GIDGoogleUser!, withError error: Error!) {
         
-        if let idToken = user.authentication?.accessToken {
-            // Submit the token to the server
-            print("Google token: \(idToken)")
+        guard let idToken = user.authentication?.accessToken else {
+            let message = "No token could be obtained from Google"
+            LogMessage(message: message, level: .error).print()
+            self.completionHandler(nil, NSError(domain: "com.mobgen.halo", code: -1, userInfo: [NSLocalizedDescriptionKey: message]))
+            return
         }
+        
+        guard let deviceAlias = Manager.core.device?.alias else {
+            let message = "No device alias could be obtained"
+            LogMessage(message: message, level: .error).print()
+            self.completionHandler(nil, NSError(domain: "com.mobgen.halo", code: -1, userInfo: [NSLocalizedDescriptionKey: message]))
+            return
+        }
+        
+        LogMessage(message: "Google token: \(idToken)", level: .info).print()
+        
+        let profile = AuthProfile(token: idToken, network: .Google, deviceId: deviceAlias)
+        authenticate(authProfile: profile, completionHandler: self.completionHandler)
     }
     
     public func sign(_ signIn: GIDSignIn!, didDisconnectWith user: GIDGoogleUser!, withError error: Error!) {
         // Remove from keychain if stored?
-    }
-
-    // MARK: SocialProvider
-    
-    public func authenticate(authProfile: AuthProfile, completionHandler handler: @escaping (User?, NSError?) -> Void) {
-        
     }
     
 }
 
 public extension SocialManager {
     
-    private var googleSocialAddon: GoogleSocialAddon? {
-        return Manager.core.addons.filter { $0 is GoogleSocialAddon }.first as? GoogleSocialAddon
-    }
-    
-    func loginWithGoogle(uiDelegate: GIDSignInUIDelegate? = nil) {
+    @objc(loginWithGoogleWithUIDelegate:completionHandler:)
+    func loginWithGoogle(uiDelegate: GIDSignInUIDelegate, completionHandler handler: @escaping (User?, Error?) -> Void) {
         
-        if let delegate = uiDelegate {
-            GIDSignIn.sharedInstance().uiDelegate = delegate
+        let google = Manager.core.addons.filter { $0 is GoogleSocialAddon }.first as? GoogleSocialAddon
+        
+        guard google != nil else {
+            let message = "No GoogleSocialAddon has been configured and registered"
+            LogMessage(message: message, level: .error).print()
+            handler(nil, NSError(domain: "com.mobgen.halo", code: -1, userInfo: [NSLocalizedDescriptionKey: message]))
+            return
         }
         
+        google?.completionHandler = handler
+        
+        GIDSignIn.sharedInstance().uiDelegate = uiDelegate
         GIDSignIn.sharedInstance().signIn()
     }
     
