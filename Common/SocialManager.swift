3f971e909e4763df6bae5dca2a15beb839c05cc7
Miguel López <miguel.lopez@mobgen.com>
2016-12-05 10:58:15 +0100

    Implements loginWithHalo method

--- Common/SocialManager.swift
+++ Common/SocialManager.swift
@@ -19,14 +19,24 @@ public class SocialManager: NSObject, HaloManager {
     }
     
     @objc(loginWithHaloAuth:completionHandler:)
-    public func loginWithHalo(authProfile: AuthProfile?, completionHandler handler: (User?, NSError?) -> Void) -> Void {
-        
+    public func loginWithHalo(authProfile: AuthProfile, completionHandler handler: @escaping (User?, NSError?) -> Void) -> Void {
+        let request = Halo.Request<User>(router: Router.loginUser(authProfile.toDictionary()))
+        try! request.responseParser(parser: userParser).responseObject { (_, result) in
+            switch (result) {
+            case .success(let user, _):
+                LogMessage(message: "Login with Halo successful.", level: .info).print()
+                handler(user, nil)
+            case .failure(let error):
+                LogMessage(message: "An error happened when trying to login with Halo.", error: error).print()
+                handler(nil, error)
+            }
+        }
     }
     
     @objc(registerWithAuthProfile:userProfile:completionHandler:)
     public func register(authProfile: AuthProfile, userProfile: UserProfile, completionHandler handler: @escaping (UserProfile?, NSError?) -> Void) -> Void {
         let request = Halo.Request<UserProfile>(router: Router.registerUser(["auth": authProfile.toDictionary(), "profile": userProfile.toDictionary()]))
-        try! request.responseParser(parser: userParser).responseObject { (_, result) in
+        try! request.responseParser(parser: userProfileParser).responseObject { (_, result) in
             switch result {
             case .success(let userProfile, _):
                 LogMessage(message: "Registration with Halo successful.", level: .info).print()
@@ -40,7 +50,14 @@ public class SocialManager: NSObject, HaloManager {
     
     // MARK : Private methods.
     
-    private func userParser(_ data: Any?) -> UserProfile? {
+    private func userParser(_ data: Any?) -> User? {
+        if let dict = data as? [String: Any] {
+            return User.fromDictionary(dict)
+        }
+        return nil
+    }
+    
+    private func userProfileParser(_ data: Any?) -> UserProfile? {
         if let dict = data as? [String: Any] {
             return UserProfile.fromDictionary(dict)
         }
